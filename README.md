427681909ce9ef9ab98696a9e65ffff7f7ee757c
Pau Fracés <pfraces@gmail.com>
2015-09-06 00:44:02 +0200

    initial release

---
+++ README.md
@@ -0,0 +1,23 @@
+domo.compiler
+=============
+
+Compile an HTML node extended with custom elements and attributes
+
+Usage
+-----
+
+```html
+<h1 model="title"></h1>
+```
+
+```js
+var grammar = {
+  model   : require('domo.grammar.model')
+};
+
+var dom   = require('domo').use({
+  compile : require('domo.compiler')(grammar)
+});
+
+dom('body').compile({ title: 'It works!' });
+```
---
+++ src/compile.js
@@ -0,0 +1,34 @@
+define('domo.compiler', function (require) {
+  'use strict';
+
+  var isFunction = require('mu.is.function'),
+      each       = require('mu.list.each'),
+      copy       = require('mu.list.copy'),
+      attr       = require('domo.attr');
+
+  var compiler = function (grammar) {
+    var compile = function (node, scope) {
+      var elementName = node.tagName.toLowerCase(),
+          attributes = attr(node),
+          breakCompilation;
+
+      var tokens = copy(attributes);
+      tokens[elementName] = true;
+
+      each(tokens, function (token) {
+        if (!isFunction(grammar[token])) { return; }
+        breakCompilation = grammar[token](node, attributes, scope, compile);
+      });
+
+      if (breakCompilation) { return; }
+
+      each(node.childNodes, function (node) {
+        compile(node, scope);
+      });
+    };
+
+    return compile;
+  };
+
+  return compiler;
+});
